b6203139c6d27dd185ea880e5cda11908de1d422
Tejam Kubde <97968546+tejamkubde@users.noreply.github.com>
2022-03-02 18:53:13 +0530

    Update jarvis.py

--- jarvis.py
+++ jarvis.py
@@ -1,11 +1,13 @@
 import queue
 import pyttsx3
-
+import webbrowser
 import speech_recognition as sr
+import pywhatkit
 
 Assistant = pyttsx3.init('sapi5')
 voices = Assistant.getProperty('voices')
 Assistant.setProperty('voices',voices[1].id)
+Assistant.setProperty("rate",170)
 
 def speak(audio):
     print("     ")
@@ -53,8 +55,39 @@ def TaskExe():
         elif "bye" in query:
             speak("bye sir")
             speak("have a good day")
+            break
 
-        
+        elif "youtube search" in query:
+            speak("Ok sir, This is what i found for your search!")
+            query = query.replace("youtube search","")
+            query = query.replace("jarvis","")
+            web = "https://www.youtube.com/results?search_query=" + query
+            webbrowser.open(web)
+            speak("Done Sir")
+            
+        elif "google search" in query:
+            speak("This is what i found for your search sir!")
+            query = query.replace("google search","")
+            query = query.replace("jarvis","")
+            pywhatkit.search(query)
+            speak("Done Sir!!")
+
+        elif "website" in query:
+            speak("Ok sir, Launching")
+            query = query.replace("website","")
+            query = query.replace("jarvis","")
+            web1 = query.replace("open","")
+            web2 = "https://www." + web1 + ".com"
+            webbrowser.open(web2)
+            speak("launched sir!")
+
+        elif "launch" in query:
+            speak("Tell me the name of the website!")
+            name = takecommand()
+            web = 'https://www.' + web + '.com'
+            webbrowser.open("https://www.google.com")
+            speak("Done Sir!")
+                
 
 
 TaskExe()
