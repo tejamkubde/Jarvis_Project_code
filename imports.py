79f03c9f89ef05902673a506a81487eedd09b1b5
Tejam Kubde <97968546+tejamkubde@users.noreply.github.com>
2022-03-06 14:45:54 +0530

    Add files via upload

---
+++ imports.py
@@ -0,0 +1,21 @@
+import queue
+import pyttsx3
+import webbrowser
+import speech_recognition as sr
+import pywhatkit
+import time
+import flask
+import os 
+import whatsapp
+import wikipedia
+import pyautogui
+from click import command
+import keyboard
+from PyDictionary import PyDictionary as diction
+import pyjokes
+import datetime
+from playsound import playsound
+import requests
+from bs4 import BeautifulSoup
+from googletrans import Translator
+from pywikihow import WikiHow , search_wikihow
