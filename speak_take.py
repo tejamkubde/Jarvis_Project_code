ea3ee9e0d91f0c02a51f7902ab43f425ebb79fc3
Tejam Kubde <97968546+tejamkubde@users.noreply.github.com>
2022-03-07 18:47:42 +0530

    Add files via upload

---
+++ speak_take.py
@@ -0,0 +1,32 @@
+import imports
+
+
+
+Assistant = imports.pyttsx3.init('sapi5')
+voices = Assistant.getProperty('voices')
+Assistant.setProperty('voices',voices[1].id)
+Assistant.setProperty("rate",170)
+
+def speak(audio):
+        print("     ")
+        Assistant.say(audio)
+
+        print(f":{audio}")
+        Assistant.runAndWait()
+        
+def takecommand():
+    command = imports.sr.Recognizer()
+    with imports.sr.Microphone() as source:
+        print("Listening.............")
+        command.pause_threshold = 1
+        audio = command.listen(source)
+        
+        try:
+            print("Recognizing............")
+            query = command.recognize_google(audio,language='en-in')
+            print(f'you said : {query}') 
+                
+        except Exception as Error:
+            return "none"
+            
+        return query.lower()
